2e2d57e412d7fffb1724057bc52affd4c5731af1
Adarshcreater <adarshprabhalkar77@gmail.com>
2024-02-16 11:54:35 +0530

    Your commit message

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+// Define a route handler for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('dateButtonClick', () => {
+    // Emit a notification event to all connected clients
+    io.emit('notification', 'Someone clicked the "Let\'s Fix a Date" button!');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
